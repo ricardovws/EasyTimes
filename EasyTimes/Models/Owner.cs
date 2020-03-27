53ce8f6ed96228cdfbbeebb25009ff30387a5b0b
Ricardo <ricardovws@gmail.com>
2020-03-27 01:03:13 -0300

    first one

---
+++ EasyTimes/Models/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyTimes.Models
+{
+    public class Owner
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
