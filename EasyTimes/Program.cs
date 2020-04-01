3831e0d052f1e3ab01ce6b3a2967ea99f70bd1f7
Ricardo <ricardovws@gmail.com>
2020-04-01 14:12:23 -0300

    almost published

--- EasyTimes/Program.cs
+++ EasyTimes/Program.cs
@@ -19,6 +19,7 @@ namespace EasyTimes
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+            .UseIISIntegration();
     }
 }
